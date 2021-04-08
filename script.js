c1b236b33b86e073ef632652d82717ff9e7161b1
Alexey Yudin <alexey.y.yudin@gmail.com>
2021-04-08 17:16:33 +0200

    code review

--- script.js
+++ script.js
@@ -3,32 +3,71 @@ const addBtn = document.querySelector(".btn-add");
 const todoItems = document.querySelector(".todoItems");
 const deleteAllBtn = document.querySelector(".btn-del-all");
 
+/** 1. Initialization
+  *   - read from localStorage
+  *   - apply the items list state
+  *   - attach the event listeners on the buttons
+ *
+ *   2. Create Button class and move the functionality of adding and removing the classes here
+ *
+ *   3. Implement event delegation and catch the delete event. Do not assign the event to the button itself.
+ *
+ *   4. Create HtmlGenerator class, which will be responsible to create html
+ *   */
+
+// const Storage = {
+//   add(key, value) {
+//     localStorage.setItem(key, JSON.stringify(value));
+//   },
+//
+//   retrieve(key) {
+//    localStorage.getItem(ke);
+//   }
+// };
+
+const buttonState = {
+  idle: 'idle',
+  active: 'active',
+};
+
+const key = 'New Todo';
+
+// TODO: use addEventListener
 inputBox.onkeyup = () => {
-    let userEnteredValue = inputBox.value; 
-    if(userEnteredValue.trim() != 0){ 
-      addBtn.classList.add("active"); 
+    let userEnteredValue = inputBox.value; // TODO: use const instead of let
+    // TODO: move the if condition to a function isValueEmpty()
+    if(userEnteredValue.trim() != 0){  // TODO: use ===
+      // addButton.setActive();
+      addBtn.classList.add(buttonState.active);
     }else{
-      addBtn.classList.remove("active"); 
+      // addButton.setIdle();
+      addBtn.classList.remove("active"); // TODO: use the buttonState enum
     }
   }
 
 
-  addBtn.onclick = () => { 
-    let userEnteredValue = inputBox.value; 
-    let getLocalStorageData = localStorage.getItem("New Todo"); 
-    if(getLocalStorageData == null){ 
-      listArray = []; 
+  // TODO: use addEventListener
+  addBtn.onclick = () => {
+    let userEnteredValue = inputBox.value; // TODO: use const
+    let getLocalStorageData = localStorage.getItem("New Todo"); // TODO: use localStorage key value
+    // const isTodoListEmpty = getLocalStorageData === null || JSON.parse(getLocalStorageData).length === 0;
+
+    let listArray;
+    if(getLocalStorageData == null){
+      listArray = [];
     }else{
-      listArray = JSON.parse(getLocalStorageData);  
+      listArray = JSON.parse(getLocalStorageData);
     }
-    listArray.push(userEnteredValue); 
-    localStorage.setItem("New Todo", JSON.stringify(listArray)); 
-  showTasks(); 
+    listArray.push(userEnteredValue);
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks();
     addBtn.classList.remove("active");
   }
-  
+
+  // TODO: split the function to many micro functions. This way we achieve Single Responsibility
   function showTasks() {
 
+      // TODO: instead of local storage use the wrapper Storage
       let getLocalStorageData = localStorage.getItem("New Todo");
       if(getLocalStorageData == null){
           listArray = [];
@@ -36,39 +75,40 @@ inputBox.onkeyup = () => {
            listArray = JSON.parse(getLocalStorageData)
           };
     const pendingTasksNumb = document.querySelector(".taskNumber");
-    pendingTasksNumb.textContent = listArray.length; 
+    pendingTasksNumb.textContent = listArray.length;
 
-        if(listArray.length > 0){ 
-              deleteAllBtn.classList.add("active"); 
+        if(listArray.length > 0){
+              deleteAllBtn.classList.add("active");
         }else{
-          deleteAllBtn.classList.remove("active"); 
+          deleteAllBtn.classList.remove("active");
         }
     let newLiTag = "";
     listArray.forEach((element, index) => {
-      newLiTag += 
+      // TODO: move the html creation to a separate class
+      newLiTag +=
           `<li>${element} 
             <div class="btn">
                <button class="btn-delete" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></button>
             </div>
             </li>`;
     });
-    todoItems.innerHTML = newLiTag; 
-    inputBox.value = ""; 
-}      
+    todoItems.innerHTML = newLiTag;
+    inputBox.value = "";
+}
 
 
 function deleteTask(index){
   let getLocalStorageData = localStorage.getItem("New Todo");
   listArray = JSON.parse(getLocalStorageData);
-  listArray.splice(index, 1); 
+  listArray.splice(index, 1);
   localStorage.setItem("New Todo", JSON.stringify(listArray));
-  showTasks(); 
+  showTasks();
 }
 
 
 deleteAllBtn.onclick = () =>{
-  listArray = []; 
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
-  showTasks(); 
+  listArray = [];
+  localStorage.setItem("New Todo", JSON.stringify(listArray));
+  showTasks();
 }
                
